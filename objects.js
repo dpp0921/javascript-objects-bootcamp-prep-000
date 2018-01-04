f9bd657adc6ccf3a2ba6c1c0c9de389cf1cb317a
Chen Ye <dpp0921@gmail.com>
2018-01-04 20:06:05 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,11 @@
+
+var playlist = {artist: "song titles"};
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist[artistName] = songTitle;
+  return playlist;
+}
+
+function removeFromPlaylist(playlst, artistName){
+  
+}
